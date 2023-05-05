a8b2b014995ec23bed7cf5e782809c86a965aa13
Javad <sir.hxt@gmail.com>
2023-05-05 11:51:37 +0330

    Initial Commit

---
+++ mydesignpatternapp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  mydesignpatternapp
+//
+//  Created by enigma 1 on 5/5/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ mydesignpatternapp/mydesignpatternappApp.swift
@@ -0,0 +1,17 @@
+//
+//  mydesignpatternappApp.swift
+//  mydesignpatternapp
+//
+//  Created by enigma 1 on 5/5/23.
+//
+
+import SwiftUI
+
+@main
+struct mydesignpatternappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
